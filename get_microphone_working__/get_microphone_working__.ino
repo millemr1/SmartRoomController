2524f7cfceee6837f9b27de141178566057633c4
Layla <Layla2@macbook-air.admin.ad.cnm.edu>
2022-07-14 15:14:35 -0500

    hope this works

--- get_microphone_working__/get_microphone_working__.ino
+++ get_microphone_working__/get_microphone_working__.ino
@@ -13,7 +13,8 @@ int PIXELCOUNT = 1;
 int PIXELPIN = 9; 
 //int i; 
 int loudnessToBrightness;
-int average;
+int averagedReadings;
+
 
 
 Adafruit_NeoPixel pixel(PIXELCOUNT, PIXELPIN,  NEO_GRB + NEO_KHZ800);
@@ -28,33 +29,37 @@ void setup() {
 
 void loop() {
  
-  ///Serial.printf("Sensor Value: %i\n" , );
-  averageMicrophoneReadings();
-  loudnessToBrightness = map(average, 0, 1024,0,64);
+  averagedReadings = averageMicrophoneReadings();
+  Serial.printf("Sensor Value Average: %i \n" , averagedReadings );
+  loudnessToBrightness = map(averagedReadings, 0, 1024,0,64);
+  Serial.printf("Mapped Value: %i \n", loudnessToBrightness);
   pixel.setBrightness(loudnessToBrightness);
   pixel.setPixelColor(0, blue);
   pixel.show();
 }
 
 int averageMicrophoneReadings(){
+  int average;
   int summation = 0;
   int min = 0;
   int max = 0;
   int i;
   int  microValue;
 
- for(i = 0; i < 100; i++){  // averages the reading from the microphone every 100 times to help level off backround noise 
-  microValue = analogRead(A9);
-  summation = microValue + summation; // adds the updated values at the top of every loop to summation
-  if(min > microValue) {  //updates minimum and maximum values 
-    min = microValue;
+
+  for(i = 0; i < 100; i++){  // averages the reading from the microphone every 100 times to help level off backround noise 
+    microValue = analogRead(A9);
+    summation = microValue + summation; // adds the updated values at the top of every loop to summation
+    if(min > microValue) {  //updates minimum and maximum values 
+       min = microValue;
     }
-   if (max < microValue){
-    max = microValue;    
+     if (max < microValue){
+       max = microValue;    
    }
     }
-    average = (summation/1000);
-    return average;
+    average = (summation/100);
+    return  average;
+    
 }
   
   
