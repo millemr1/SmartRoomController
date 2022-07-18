4f8b169f4f74d66be1560643ee9a5dc3b334c12a
Layla <Layla2@macbook-air.admin.ad.cnm.edu>
2022-07-18 17:30:12 -0500

    UGH

--- Getwemoestoturnonatcertaintimes/Getwemoestoturnonatcertaintimes.ino
+++ Getwemoestoturnonatcertaintimes/Getwemoestoturnonatcertaintimes.ino
@@ -12,8 +12,9 @@
 
 
 //int currentTimeforWemos;
-int specficTime;
+int specTime;
 int currentTime;
+bool timesMatched;
 bool status;
 byte thisbyte;  //for ip address
 void setup() {
@@ -24,7 +25,7 @@ void setup() {
   digitalWrite(10, HIGH);
   status = Ethernet.begin(mac); //begin ethernet connection
   delay(200);
-  printIP();
+//  printIP();
   if (!status) {
     Serial.printf("failed to configure Ethernet using DHCP \n");
     //no point in continueing 
@@ -40,14 +41,14 @@ void setup() {
 void loop() {
   //currentTimeforWemos = millis();
   currentTime = getCurrentTime();  //somehow this feels redundant
-  specficTime = setSpecifiedTime(16,20,01);  //somehow this also feels redundant
-  timesMatched = DoTimesMatch(currentTime, specificTime);
-  if(timesMatched(currentTime, specificTime){   //Keep wemo on for a few minutes would be something like 
+  specTime= setSpecifiedTime(16,20,01);  //somehow this also feels redundant
+  timesMatched = DoTimesMatch(currentTime, specTime);
+  if(DoTimesMatch(currentTime, specTime){   //Keep wemo on for a few minutes would be something like 
      switchON(3);
   }
   //else();
  // switchOFF(3);
-  }
+  
 
 int getCurrentTime(){ //try to write to get it on at anytime I pass into the function
   int currentTime;
@@ -55,13 +56,13 @@ int getCurrentTime(){ //try to write to get it on at anytime I pass into the fun
   int hours = hour();
   int minutes = minute();
   int seconds = second();  //might be extraneous?
-  currentTime = hour + minute + second;
+  currentTime = hours + minutes + seconds;
   Serial.printf("Time: %i : %i, %i", hours, minutes, seconds);  // have it display to screen eventually?
     return currentTime;
   }
-int setSpecificiedTime(int hours, int minutes, int seconds){  //I am quite unsure what I am doing here
-  int specficTime = hours + minutes + seconds
-  return specficiedTime
+int setSpecifiedTime(int hours, int minutes, int seconds){  //I am quite unsure what I am doing here
+  int certainficTime = hours + minutes + seconds
+  return certainTime;
 }
 bool DoTimesMatch(int time1, int time2){
   bool timesMatch;
@@ -74,13 +75,6 @@ bool DoTimesMatch(int time1, int time2){
  return timesMatch;
 }
 
-void printIP() {
-  Serial.printf("My IP address: ");
-  for (byte thisByte = 0; thisByte < 3; thisByte++) {
-  Serial.printf("%i.",Ethernet.localIP()[thisByte]);
- }
- Serial.printf("%i\n",Ethernet.localIP()[3]);
-}
 void printIP() {
   Serial.printf("My IP address: ");
   for (byte thisByte = 0; thisByte < 3; thisByte++) {
