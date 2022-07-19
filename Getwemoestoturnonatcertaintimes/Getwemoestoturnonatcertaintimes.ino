607d872b6d8d05bf701b461231e1c861d23d285f
Layla <Layla2@macbook-air.admin.ad.cnm.edu>
2022-07-19 09:19:59 -0500

    this is updated

--- Getwemoestoturnonatcertaintimes/Getwemoestoturnonatcertaintimes.ino
+++ Getwemoestoturnonatcertaintimes/Getwemoestoturnonatcertaintimes.ino
@@ -3,6 +3,7 @@
  * Author:  Micalah Miller
  * July-14-2022
 */
+// I want to use time to turn on wemos (2 smart outlets on twice a day);
 
 
 #include<SPI.h>
@@ -13,13 +14,24 @@
 
 //int currentTimeforWemos;
 int specTime;
-int currentTime;
+int currentTiming;
 bool timesMatched;
 bool status;
 byte thisbyte;  //for ip address
 void setup() {
   
+   // set the Time library to use Teensy 3.0's RTC to keep time
+  setSyncProvider(getTeensy3Time);  //what goes through here?
+  
   Serial.begin(9600);
+  while (!Serial);  // Wait for Arduino Serial Monitor to open
+  delay(100);
+  if (timeStatus()!= timeSet) {
+    Serial.println("Unable to sync with the RTC");
+  } else {
+    Serial.println("RTC has set the system time");
+  }
+}
   
   pinMode(10, OUTPUT);  // enables serial monitor pin
   digitalWrite(10, HIGH);
@@ -40,28 +52,27 @@ void setup() {
 }
 void loop() {
   //currentTimeforWemos = millis();
-  currentTime = getCurrentTime();  //somehow this feels redundant
-  specTime= setSpecifiedTime(16,20,01);  //somehow this also feels redundant
-  timesMatched = DoTimesMatch(currentTime, specTime);
-  if(DoTimesMatch(currentTime, specTime){   //Keep wemo on for a few minutes would be something like 
+  currentTiming = getCurrentTime();  //somehow this feels redundant
+  specTime= setSpecifiedTime(17,49,01);  //somehow this also feels redundant
+  //timesMatched = DoTimesMatch(currentTiming, specTime);
+  if(DoTimesMatch(currentTiming, specTime)){   //Keep wemo on for a few minutes would be something like 
      switchON(3);
   }
   //else();
  // switchOFF(3);
   
-
+}
 int getCurrentTime(){ //try to write to get it on at anytime I pass into the function
   int currentTime;
-  int wemoNumber;  //three is tea Kettle for your functionality
   int hours = hour();
   int minutes = minute();
   int seconds = second();  //might be extraneous?
   currentTime = hours + minutes + seconds;
-  Serial.printf("Time: %i : %i, %i", hours, minutes, seconds);  // have it display to screen eventually?
+  Serial.printf("Time: %i : %i, : %i \n", hours, minutes, seconds);  // have it display to screen eventually?
     return currentTime;
   }
 int setSpecifiedTime(int hours, int minutes, int seconds){  //I am quite unsure what I am doing here
-  int certainficTime = hours + minutes + seconds
+  int certainTime = hours + minutes + seconds;
   return certainTime;
 }
 bool DoTimesMatch(int time1, int time2){
