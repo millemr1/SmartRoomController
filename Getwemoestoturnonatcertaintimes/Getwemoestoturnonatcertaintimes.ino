ff0e389b775db5508a324a905cd0715153ca89fb
Layla <Layla2@macbook-air.admin.ad.cnm.edu>
2022-07-19 10:14:44 -0500

    something is wrong

--- Getwemoestoturnonatcertaintimes/Getwemoestoturnonatcertaintimes.ino
+++ Getwemoestoturnonatcertaintimes/Getwemoestoturnonatcertaintimes.ino
@@ -21,7 +21,7 @@ byte thisbyte;  //for ip address
 void setup() {
   
    // set the Time library to use Teensy 3.0's RTC to keep time
-//  setSyncProvider(getTeensy3Time);  //what goes through here?
+ setSyncProvider(getTeensy3Time);  
   
   Serial.begin(9600);
   while (!Serial);  // Wait for Arduino Serial Monitor to open
@@ -37,29 +37,22 @@ void setup() {
   status = Ethernet.begin(mac); //begin ethernet connection
   delay(200);
 //  printIP();
-  if (!status) {
-    Serial.printf("failed to configure Ethernet using DHCP \n");
-    //no point in continueing 
-    while(1);
-  }
-  Serial.print("My IP address: ");    //get local IP address for the room, somehow. not quite sure why we use this incrementing.
-  for (thisbyte = 0; thisbyte < 3; thisbyte++) {
-    //print value of each byte of the IP address
-    Serial.printf("%i.",Ethernet.localIP()[thisbyte]);
-    }
- Serial.printf("%i\n",Ethernet.localIP()[thisbyte]);
+  
 }
 void loop() {
-  //currentTimeforWemos = millis();
+  
   currentTiming = getCurrentTime();  //somehow this feels redundant
-  specTime= setSpecifiedTime(17,49,01);  //somehow this also feels redundant
+  specTime= setSpecifiedTime(9,59,00);  //somehow this also feels redundant
   //timesMatched = DoTimesMatch(currentTiming, specTime);
   if(DoTimesMatch(currentTiming, specTime)){   //Keep wemo on for a few minutes would be something like 
-     switchON(3);
+     Serial.printf("times match");
+     switchON(4);
+
+     
   }
   //else();
  // switchOFF(3);
-  
+  digitalClockDisplay();  
 }
 int getCurrentTime(){ //try to write to get it on at anytime I pass into the function
   int currentTime;
@@ -85,14 +78,54 @@ bool DoTimesMatch(int time1, int time2){
  return timesMatch;
 }
 
-void printIP() {
-  Serial.printf("My IP address: ");
-  for (byte thisByte = 0; thisByte < 3; thisByte++) {
-  Serial.printf("%i.",Ethernet.localIP()[thisByte]);
- }
- Serial.printf("%i\n",Ethernet.localIP()[3]);
+time_t getTeensy3Time() {
+  return Teensy3Clock.get();
 }
-  
+/*  code to process time sync messages from the serial port   */
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+unsigned long processSyncMessage() {
+  unsigned long pctime = 0L;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013 
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     return pctime;
+     if( pctime < DEFAULT_TIME) { // check the value is a valid time (greater than Jan 1 2013)
+       pctime = 0L; // return 0 to indicate that the time is not valid
+     }
+  }
+  return pctime;
+}
+
+void digitalClockDisplay() {
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+void turnWemoOffAfter3Minutes(int wemoNumber){ //turn Wemo on for 3 minutes
+  int currentTimeforWemos = millis();
+  int lastTimeforWemos;
+  if(currentTimeforWemos - lastTimeforWemos > 180000) {
+    switchOFF(wemoNumber);
+    lastTimeforWemos = millis();
+    } 
+}
 
 
   
